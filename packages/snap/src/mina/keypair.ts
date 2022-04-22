ad38ddb7338a237bd9ecd4831fdea0865d1798bd
Marin Petrunic <marin.petrunic@gmail.com>
2022-04-22 23:48:57 +0200

    fix another matijas comment

--- packages/snap/src/mina/keypair.ts
+++ packages/snap/src/mina/keypair.ts
@@ -1,7 +1,6 @@
-import type { SnapProvider } from "@metamask/snap-types";
 import Client from "mina-signer";
 import { Keypair } from "mina-signer/dist/src/TSTypes";
 
-export function getKeypair(client: Client, wallet: SnapProvider): Keypair {
+export function getKeypair(client: Client): Keypair {
   return client.genKeys();
 }
