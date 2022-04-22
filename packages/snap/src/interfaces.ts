eeeef8245b93543ed2d7052d513b866467167950
Marin Petrunić <mpetrunic@users.noreply.github.com>
2022-04-22 23:49:36 +0200

    feat: get public key (#7)
    * add address fetching
    
    * fix matijas comment
    
    * fix another matijas comment

--- packages/snap/src/interfaces.ts
+++ packages/snap/src/interfaces.ts
@@ -11,7 +11,3 @@ export const EmptyMetamaskState: () => MetamaskState = () => ({
     network: "mainnet",
   },
 });
-
-export interface Wallet {
-  request(options: { method: string; params?: unknown[] }): unknown;
-}
