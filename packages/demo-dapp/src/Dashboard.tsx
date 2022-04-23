246157f88231a00d9a771d98ac6ea68e0740df4e
Matija Petrunic <matija.petrunic@fer.hr>
2022-04-23 19:58:59 +0200

    Remove console logs

--- packages/demo-dapp/src/Dashboard.tsx
+++ packages/demo-dapp/src/Dashboard.tsx
@@ -82,7 +82,6 @@ export const Dashboard: FC = () => {
     setIsLoading(true)
     try {
       const sendTransactionResponse = (await sendTransaction(sendTxData)) as ITransactionResponse;
-      console.log(sendTransactionResponse)
       if (sendTransactionResponse.error !== null) {
         throw new Error("Error");
       }
@@ -96,8 +95,6 @@ export const Dashboard: FC = () => {
       setIsLoading(false);
       toaster.success(`Sent transaction: ${sendTransactionResponse.tx.result.payment.hash}`);
     } catch(error) {
-      console.log(error)
-
       setIsLoading(false)
       toaster.danger("Error signing message!");
     }
