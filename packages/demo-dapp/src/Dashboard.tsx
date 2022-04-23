6f9114c55141ddb683a00b62bf49baf40d83a5a6
Marin Petrunic <marin.petrunic@gmail.com>
2022-04-23 18:06:17 +0200

    check metamask installation

--- packages/demo-dapp/src/Dashboard.tsx
+++ packages/demo-dapp/src/Dashboard.tsx
@@ -1,14 +1,15 @@
-import { Button, Spinner, TextInput, toaster } from "evergreen-ui";
+import { Button, Spinner, TextInput, TickCircleIcon, toaster } from "evergreen-ui";
 import { FC, useEffect, useState } from "react";
 import { ReactComponent as Logo } from './assets/logo.svg';
 import cls from "classnames";
-import { enableSnap, getAccount, getSignMessage, sendTransaction } from "./services/snap";
+import { enableSnap, getAccount, getSignMessage, isMetamaskSnapsSupported, sendTransaction } from "./services/snap";
 import { ISignMessageResponse } from "./types";
-import {VerifyMessage} from "./components/VerifyMessage";
+import { VerifyMessage } from "./components/VerifyMessage";
 import useInterval from "use-interval";
 
 export const Dashboard: FC = () => {
   const [snapConnected, setSnapConnected] = useState(false);
+  const [isFlaskInstalled, setFlaskInstalled] = useState(false);
   // TODO
   const [userAddress, setUserAddress] = useState("loading...")
   const [userBalance, setUserBalance] = useState("loading...")
@@ -36,14 +37,14 @@ export const Dashboard: FC = () => {
   }, 30000)
 
   useEffect(() => {
-    if(!snapConnected) return;
+    if (!snapConnected) return;
     (async function () {
       setIsLoading(true)
       try {
         const account = await getAccount();
         setUserAddress(account.account.publicKey);
         setUserBalance(account.account.balance.total);
-      } catch(e: any) {
+      } catch (e: any) {
         console.error(e)
         toaster.danger(e.message)
       }
@@ -51,11 +52,28 @@ export const Dashboard: FC = () => {
     })()
   }, [snapConnected])
 
+  useEffect(() => {
+    (async function () {
+      try {
+        const result = await isMetamaskSnapsSupported()
+        setFlaskInstalled(result)
+      } catch (e: any) {
+        console.error(e)
+      }
+    })()
+  }, [isFlaskInstalled])
+
   const onConnect = async () => {
     setIsLoading(true)
-    const isConnected = await enableSnap()
-    setIsLoading(false)
-    setSnapConnected(isConnected)
+    try {
+      const isConnected = await enableSnap()
+      setSnapConnected(isConnected)
+    } catch (e) {
+
+    } finally {
+      setIsLoading(false)
+    }
+
   }
 
   //--SIGN MESSAGE
@@ -64,10 +82,10 @@ export const Dashboard: FC = () => {
   }
   const signMessage = async () => {
     setIsLoading(true)
-    try{
+    try {
       const signMessageResponse = (await getSignMessage(signMessageData)) as ISignMessageResponse;
       setIsLoading(false)
-      if(signMessageResponse.confirmed === false) {
+      if (signMessageResponse.confirmed === false) {
         toaster.warning("Message not confirmed")
       }
       toaster.success(`Message signed successfully!: ${signMessageResponse.signature.data.message}`);
@@ -79,7 +97,7 @@ export const Dashboard: FC = () => {
 
   //--SEND TRANSACTION
   const sendTxOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSendTxData({...sendTxData, [e.target.name]: e.target.value})
+    setSendTxData({ ...sendTxData, [e.target.name]: e.target.value })
   }
   const sendTx = async () => {
     setIsLoading(true)
@@ -91,36 +109,38 @@ export const Dashboard: FC = () => {
 
   //--VERIFY MESSAGE
   const verifyMessageOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setVerifyMessageData({...verifyMessageData, [e.target.name]: e.target.value})
+    setVerifyMessageData({ ...verifyMessageData, [e.target.name]: e.target.value })
   }
   const verifyMessage = () => {
     //TODO
     toaster.success("Message verified");
   }
-  //TODO--user data
-  // useEffect(() => {
-  //   (async () => {
-  //     // @ts-ignore
-  //     await window.ethereum.request({
-  //       method: 'wallet_invokeSnap',
-  //       params: [
-  //         defaultSnapId,
-  //         { method: "key" }
-  //       ]
-  //     });
-  //   })();
-  // }, [])
 
   return <div className="dashboard">
     {
-      isLoading && <div className="spinner-container"><Spinner size={100}/></div>
+      isLoading && <div className="spinner-container"><Spinner size={100} /></div>
     }
     <header className={cls({ connected: snapConnected })}>
       <h1 >Mina Snap</h1>
       <Logo className={cls({ connected: snapConnected })} />
     </header>
     {!snapConnected ?
-      <Button disabled={isLoading} className="connect-button" onClick={() => onConnect()}>Connect snap</Button> :
+      <>
+        {
+          isFlaskInstalled ?
+            <Button disabled={true} className="connect-button">Metamask Flask Installed <TickCircleIcon style={{marginLeft: "0.1“rem"}} color="success" marginRight={16} /></Button>
+            :
+            <a
+              className="connect-button"
+              href="https://github.com/ChainSafe/mina-snap/releases/tag/flask-demo"
+              target="_blank" rel="noreferrer"
+            >
+              <Button className="connect-button">Install Metamask Flask</Button>
+            </a>
+        }
+
+        <Button disabled={isLoading || !isFlaskInstalled} className="connect-button" onClick={() => onConnect()}>Connect snap</Button>
+      </> :
       <div className="dashboard-connected">
         <div className="row">
           <div className="user-data box">
@@ -135,24 +155,24 @@ export const Dashboard: FC = () => {
           </div>
           <div className="sign-message box">
             <h2>Sign message</h2>
-            <TextInput name="signMessage" onChange={signMessageOnChange}/>
+            <TextInput name="signMessage" onChange={signMessageOnChange} />
             <Button disabled={isLoading} onClick={signMessage}>Sign</Button>
           </div>
         </div>
         <div className="row">
-        <div className="send-tx box">
-          <h2>Send message</h2>
-          <h3>To</h3>
-          <TextInput name="to" onChange={sendTxOnChange}/>
-          <h3>Memo</h3>
-          <TextInput name="memo" onChange={sendTxOnChange}/>
-          <h3>Amount</h3>
-          <TextInput name="amount" onChange={sendTxOnChange}/>
-          <h3>Fee</h3>
-          <TextInput name="fee" onChange={sendTxOnChange}/>
-          <Button disabled={isLoading} onClick={sendTx}>Send</Button>
-        </div>
-        <VerifyMessage />
+          <div className="send-tx box">
+            <h2>Send message</h2>
+            <h3>To</h3>
+            <TextInput name="to" onChange={sendTxOnChange} />
+            <h3>Memo</h3>
+            <TextInput name="memo" onChange={sendTxOnChange} />
+            <h3>Amount</h3>
+            <TextInput name="amount" onChange={sendTxOnChange} />
+            <h3>Fee</h3>
+            <TextInput name="fee" onChange={sendTxOnChange} />
+            <Button disabled={isLoading} onClick={sendTx}>Send</Button>
+          </div>
+          <VerifyMessage />
         </div>
 
       </div>
